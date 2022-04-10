dceaf94a13bc8acf324ac3caa7607a96c9a5fc0b
Mubashir Ansari <mubassheransari@gmail.com>
2022-04-10 18:09:03 +0500

    android with fastlane

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.angularboilerplate.app',
+  appName: 'angular-boilerplate',
+  webDir: 'dist/angular-boilerplate',
+  bundledWebRuntime: false
+};
+
+export default config;
